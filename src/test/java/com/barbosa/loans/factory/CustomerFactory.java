e8c5e06a507907f323ed96a19137a8b0da50b7b1
adriano barbosa <adrianobarbosa_4@hotmail.com>
2024-09-14 17:55:36 -0300

    finalizando testes pacote domain

--- src/test/java/com/barbosa/loans/factory/CustomerFactory.java
+++ src/test/java/com/barbosa/loans/factory/CustomerFactory.java
@@ -11,5 +11,13 @@ public class CustomerFactory {
     public static Customer build(Double income){
         return  new Customer(25, "123.456.789-00", "Adriano", income, "SP");
     }
+
+    public static Customer build(Integer age){
+        return  new Customer(age, "123.456.789-00", "Adriano", 5000.00, "SP");
+    }
+
+    public static Customer build(String location){
+        return  new Customer(25, "123.456.789-00", "Adriano", 5000.00, location);
+    }
 }
 
